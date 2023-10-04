408dc02bea8715a0ce37b5176d019985ee8045a1
ivini95 <viniximenesdev@gmail.com>
2023-10-04 19:53:38 -0300

    refactor: isolate component taskToday and status task

--- app/statusToday/page.tsx
+++ app/statusToday/page.tsx
@@ -1,14 +1,11 @@
 import Link from "next/link";
 import { Button } from "../components/ui/button";
+import InfoStatusToday from "../components/statusTodayContainer/infoStatusToday";
 
 export default function StatusToday() {
     return (
         <div className="flex flex-col items-center justify-center h-screen gap-8">
-            <div className="flex flex-col items-center justify-center gap-4">
-                <div className="flex justify-between w-72"><p>Total de Tarefas</p><span>03 / 20</span></div>
-                <div className="flex justify-between w-72"><p>Tempo a Cumprir</p><span>01 : 20</span></div>
-                <div className="flex justify-between w-72"><p>Tempo Decorrido</p><span>03 : 10</span></div>
-            </div>
+            <InfoStatusToday/>
             <Link href={'tasksToday'}><Button>TAREFAS</Button></Link>
         </div>
     )
--- app/tasksToday/page.tsx
+++ app/tasksToday/page.tsx
@@ -1,32 +1,12 @@
 import Link from "next/link";
 import { Button } from "../components/ui/button";
 import { Checkbox } from "../components/ui/checkbox";
+import TasksTodayList from "../components/tasksTodayContainer/tasksTodayList";
 
 export default function TasksToday() {
     return (
         <div className="flex flex-col items-center justify-between h-screen gap-8">
-            <div className="flex flex-col gap-2 h-80 mt-16">
-                <div className="flex flex-col items-center justify-between w-72 h-28">
-                    <div className="flex w-full justify-between p-1">
-                        <Checkbox/>
-                        <div>00 : 30 : 00</div>
-                    </div>
-                    <div className="flex w-full items-center justify-between p-1">
-                        <p>description of task</p>
-                        <Link href={'/task'}><Button className="w-16 h-8">Play/pause</Button></Link>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center justify-between w-72 h-28">
-                    <div className="flex w-full justify-between p-1">
-                        <Checkbox/>
-                        <div>00 : 30 : 00</div>
-                    </div>
-                    <div className="flex w-full items-center justify-between p-1">
-                        <p>description of task</p>
-                        <Button className="w-16 h-8">Play/pause</Button>
-                    </div>
-                </div>
-            </div>
+            <TasksTodayList/>
             <div className="flex justify-center gap-4 w-72 mb-4">
                 <Link href={'/stopwatch'}><Button>CRONOMETRO</Button></Link>
                 <Link href={'/timer'}><Button>TEMPORIZADOR</Button></Link>
