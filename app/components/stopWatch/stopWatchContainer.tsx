dd4c0e87a8c70c8c2dd86bb8329da14aa86f7238
ivini95 <viniximenesdev@gmail.com>
2023-10-05 15:09:56 -0300

    style: styled components of stopWatch page

---
+++ app/components/stopWatch/stopWatchContainer.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../ui/button";
+
+export default function StopwatchContainer() {
+    return (
+            <div className="flex flex-col items-center justify-around p-4 h-80 w-64 absolute top-1/4">
+            <div>00 : 23 : 02</div>
+                <p>Description task</p>
+                <Button>Pause/Play</Button>
+            </div>
+    )
+}
