c7d8efc3fc6214d4e2a98863ef0222e1c354037e
ivini95 <viniximenesdev@gmail.com>
2023-10-05 15:06:33 -0300

    style: styled components of create task page

---
+++ app/components/createTask/descriptionTask.tsx
@@ -0,0 +1,6 @@
+
+export default function DescriptionTask() {
+    return (
+        <input data-testid="inputDescriptionTask" className="h-10 w-80 rounded-2xl text-center" type="text" name="description" id="description" placeholder="Descrição" />
+    )
+}
