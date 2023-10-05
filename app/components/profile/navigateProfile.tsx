4c1604d09536282178fbcb11707c068888c2c8aa
ivini95 <viniximenesdev@gmail.com>
2023-10-05 15:10:18 -0300

    style: styled components of page

---
+++ app/components/profile/navigateProfile.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export default function NavigateProfile() {
+    return(
+        <div className="flex flex-col gap-4">
+          <Link href={'/statusToday'}><Button>COMEÇAR</Button></Link>
+          <Link href={'/createTask'}><Button>CRIAR TAREFA</Button></Link>
+          <Link href={'/management'}><Button>GERENCIAR</Button></Link>
+        </div>
+    )
+}
