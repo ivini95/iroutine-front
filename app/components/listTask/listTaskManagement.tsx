bf04b704bc2913d6bcf764db120fa98c91e854b8
ivini95 <viniximenesdev@gmail.com>
2023-10-06 18:10:49 -0300

    style: position componenets in container

--- app/components/listTask/listTaskManagement.tsx
+++ app/components/listTask/listTaskManagement.tsx
@@ -3,11 +3,16 @@ import { Button } from '../ui/button';
 
 export default function ListTaskManagement() {
   return (
-    <div>
-      <p>Description task</p>
-      <Link href={'editTask'}>
-        <Button data-testid="editTask"></Button>
-      </Link>
+    <div className="flex flex-col items-center mt-28 gap-8">
+      <div className="flex flex-col items-center justify-between w-80 h-20 bg-slate-300 p-2 rounded-2xl">
+        <div className='flex items-center justify-between w-full'>
+          <p>00 : 30 : 00</p>
+          <Link href={'editTask'}>
+            <Button className='w-12 h-6' data-testid="editTask">Edit</Button>
+          </Link>
+        </div>
+        <p>Description task</p>
+      </div>
     </div>
   );
 }
--- app/listTask/page.tsx
+++ app/listTask/page.tsx
@@ -2,7 +2,7 @@ import ListTaskManagement from "../components/listTask/listTaskManagement";
 
 export default function ListTask() {
     return (
-        <div>
+        <div  className="flex flex-col items-center h-screen ">
             <ListTaskManagement/>
         </div>
     )
