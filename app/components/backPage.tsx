2de202e531ba2c8213dbdaac3435a3bf5cf819bc
ivini95 <viniximenesdev@gmail.com>
2023-10-09 17:13:35 -0300

    dinamic title added

--- app/components/backPage.tsx
+++ app/components/backPage.tsx
@@ -1,16 +1,27 @@
 'use client'
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter,  } from 'next/navigation';
 import { Button } from './ui/button';
 import { ArrowLeft } from 'lucide-react';
+import { NextRequest } from 'next/server';
 
-export default function BackPage() {
+export default function BackPage(request: NextRequest) {
   const router = useRouter();
+  let pathname = usePathname()
+
+  let titleParts = pathname.split('/')
+
+  titleParts.shift()
+
+  let titleTogether = titleParts[0][0].toUpperCase() + titleParts[0].slice(1);
+
+  let title = titleTogether.replace(/([a-z])([A-Z])/g, '$1 $2');  
+  
   return (
     <div className='flex items-center w-full absolute top-12'>
       <Button className='h-6 w-12 ml-4' type="button" onClick={() => router.back()}>
         <ArrowLeft size={16}/>
       </Button>
-      <h2 className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>page tittle</h2>
+      <h2 className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>{title}</h2>
     </div>
   );
 }
