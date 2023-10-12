e84ff307faa4cda41dc18d7cf9b4c5dee85f0853
ivini95 <viniximenesdev@gmail.com>
2023-10-12 15:25:54 -0300

    test: test home page and its components

---
+++ __tests__/pages/home.test.tsx
@@ -0,0 +1,26 @@
+import Home from "@/app/page"
+import { fireEvent, render, screen } from "@testing-library/react"
+
+import {MemoryRouterProvider} from 'next-router-mock/MemoryRouterProvider'
+import mockRouter from 'next-router-mock'
+
+describe('home page', () => {
+    it('should render title page', () => {
+        render(<Home/>)
+
+        expect(screen.getByText('IROUTINE')).toBeInTheDocument()
+    })
+
+    it('should render button login', () => {
+        render(<Home/>)
+
+        expect(screen.getByRole('button')).toBeInTheDocument()
+    })
+
+    it('should redirect to profile page', () => {
+        render(<Home/>, {wrapper: MemoryRouterProvider})
+
+        fireEvent.click(screen.getByRole('button'))
+        expect(mockRouter.asPath).toEqual('/profile')
+    })
+})
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import {Button} from '../app/components/ui/button'
 
 export default function Home() {
   return (
     <main className="h-screen flex flex-col items-center justify-center gap-12">
-      <Button className="h-16 w-40 font-semibold text-sm">Login</Button>
+      <Link href={'profile'}><Button className="h-16 w-40 font-semibold text-sm">Login</Button></Link>
       <h1 className='font-semibold'>IROUTINE</h1>
     </main>
   )
