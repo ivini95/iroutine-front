b99f51c5b491bcfcbd5d1b81fa6703af2cdd4277
ivini95 <viniximenesdev@gmail.com>
2023-10-12 15:13:14 -0300

    feat: create login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import {Button} from '../app/components/ui/button'
+
 export default function Home() {
   return (
-    <main className="h-screen">
-      <h1>IROUTINE</h1>
+    <main className="h-screen flex flex-col items-center justify-center gap-12">
+      <Button className="h-16 w-40 font-semibold text-sm">Login</Button>
+      <h1 className='font-semibold'>IROUTINE</h1>
     </main>
   )
 }
