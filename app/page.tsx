fcd862d47c59055ca1913bc80c4a1f73e4e95c6a
ivini95 <viniximenesdev@gmail.com>
2023-10-16 16:46:30 -0300

    refact: redirect modify to login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link'
 import {Button} from '../app/components/ui/button'
 
+
+//adionar imagens para ilustrar funções
 export default function Home() {
   return (
     <main className="h-screen flex flex-col items-center justify-center gap-12">
-      <Link href={'profile'}><Button className="h-16 w-40 font-semibold text-sm">Login</Button></Link>
-      <h1 className='font-semibold'>IROUTINE</h1>
+      <p>Iroutine é um gerenciador de tarefas em lista simples.</p>
+      <p>Adicione tarefas a sua lista diaria de tarefas para melhor visualizar o que tem a fazer.</p>
+      <p>Agende um tempo de conclusão ou contador para analise do tempo gasto durante seu dia.</p>
+      <p>Função de cronometro e contador para tarefas fora da lista.</p>
+      <Link href={'login'}><Button className="h-16 w-40 font-semibold text-sm">Login</Button></Link>
     </main>
   )
 }
