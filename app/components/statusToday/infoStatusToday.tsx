7abc4a40cd25d44f311b16b88ce5e60beb86c775
ivini95 <viniximenesdev@gmail.com>
2023-11-02 16:37:37 -0300

    feat: create button create task in status today, button pops up when user don't have any tasks

--- app/components/statusToday/infoStatusToday.tsx
+++ app/components/statusToday/infoStatusToday.tsx
@@ -1,4 +1,6 @@
 export default function InfoStatusToday() {
+    //buscar dados das tarefas
+    //tratar dados de tarefas e mostrar
     return (
         <div className="flex flex-col items-center justify-center gap-4">
             <div className="bg-primary flex items-center p-2 justify-between h-20 w-80 rounded-lg shadow-tg text-sm"><p className="font-medium text-background">Total de Tarefas</p><span className="font-medium text-background text-sm">03 / 20</span></div>
--- app/statusToday/page.tsx
+++ app/statusToday/page.tsx
@@ -1,14 +1,59 @@
-import Link from "next/link";
-import { Button } from "../components/ui/button";
-import InfoStatusToday from "../components/statusToday/infoStatusToday";
-import BackPage from "../components/backPage";
+'use client';
+
+import Link from 'next/link';
+import { Button } from '../components/ui/button';
+import InfoStatusToday from '../components/statusToday/infoStatusToday';
+import BackPage from '../components/backPage';
+import Cookie from 'js-cookie';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function StatusToday() {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen gap-8">
-            <BackPage/>
-            <InfoStatusToday/>
-            <Link href={'tasksToday'}><Button className="h-16 w-40 font-semibold text-sm shadow-tg">TAREFAS</Button></Link>
+
+  const router = useRouter()
+  const [tasks, setTasks] = useState([])
+
+  const userId = Cookie.get('user_id');
+  const authToken = Cookie.get('auth_token');
+
+    async function getTaskData() {
+        const res = await fetch(`http://localhost:3000/task/${userId}`, {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          });
+        
+          const taskData = await res.json();
+          setTasks(taskData)
+    }
+
+  
+
+  useEffect(() => {
+    getTaskData()
+  },[])
+
+  
+  function redirectToCreateTask() {
+    
+    router.push('/createTask')
+  }
+
+  return (
+    tasks?.length < 1 ? (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <Button onClick={redirectToCreateTask} className="h-16 w-40 font-semibold text-sm shadow-tg">Criar Tarefa</Button>
         </div>
-    )
+      ) : (
+        <div className="flex flex-col items-center justify-center h-screen gap-8">
+          <BackPage />
+          <InfoStatusToday />
+          <Link href={'tasksToday'}>
+            <Button className="h-16 w-40 font-semibold text-sm shadow-tg">
+              TAREFAS
+            </Button>
+          </Link>
+        </div>
+      )
+  )
 }
