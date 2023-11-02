a381b160cf75e86def110094e921d55792b1d7ac
ivini95 <viniximenesdev@gmail.com>
2023-11-02 15:00:12 -0300

    refactor: create rule when user not exist or is unauthorizes

--- app/components/profile/salutationContainer.tsx
+++ app/components/profile/salutationContainer.tsx
@@ -1,8 +1,60 @@
+'use client';
+import Cookie from 'js-cookie';
+import { useEffect, useState } from 'react';
+
+interface IdataRes {
+  name: string;
+  id: string;
+}
+
 export default function SalutionContainer() {
-    return(
-        <div className="flex flex-col items-center justify-center">
-          <h2 className="text-base font-semibold">Olá<span> Vinicius</span>!</h2>
-          <p className="text-sm text-primary font-semibold">Boa tarde.</p>
-        </div>
-    )
+  const [dataRes, setDataRes] = useState<null | IdataRes>(null);
+  const [currentSalution, setCurrentSalution] = useState<string>('');
+
+  async function getUserData() {
+    const userId = Cookie.get('user_id');
+    const authToken = Cookie.get('auth_token');
+    const res = await fetch(`http://localhost:3000/users/${userId}`, {
+      cache: 'no-store',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+    const data = await res.json();
+    setDataRes(data);
+  }
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  function getCurrentData() {
+    const newDate = new Date();
+    const hour = newDate.getHours();
+
+    if (hour >= 6 && hour < 12) {
+      setCurrentSalution('Bom Dia!');
+    } else if (hour >= 12 && hour <= 18) {
+      setCurrentSalution('Boa Tarde!');
+    } else if (hour > 18 && hour <= 24) {
+      setCurrentSalution('Boa Noite!');
+    } else {
+      setCurrentSalution('Tá meio tarde né.');
+    }
+  }
+
+  useEffect(() => {
+    getCurrentData();
+  }, []);
+
+  //caso não esteja authorizado apagar cookie e redirecionar para login
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="text-base font-semibold">
+        Olá<span> {dataRes?.name}</span>!
+      </h2>
+      <p className="text-sm text-primary font-semibold">{currentSalution}</p>
+    </div>
+  );
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,14 +1,61 @@
-import NavigateProfile from "../components/profile/navigateProfile";
-import SalutionContainer from "../components/profile/salutationContainer";
+'use client';
 
+import { useEffect, useState } from 'react';
+import NavigateProfile from '../components/profile/navigateProfile';
+import SalutionContainer from '../components/profile/salutationContainer';
+import Cookie from 'js-cookie';
+import { useRouter } from 'next/navigation';
 
 export default function Profile() {
-  return (
+  const [message, setMessage] = useState<null | string>(null);
+
+  const router = useRouter();
+
+  async function getUserData() {
+    const userId = Cookie.get('user_id');
+    const authToken = Cookie.get('auth_token');
+    const res = await fetch(`http://localhost:3000/users/${userId}`, {
+      cache: 'no-store',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+    var data = await res.json();
+    if (!res.ok) {
+      if (typeof data.message == 'string') {
+        setMessage(data.message);
+      } else {
+        setMessage(data.message[0]);
+      }
+    }
+  }
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  useEffect(() => {
+    if (message) {
+      Cookie.remove('auth_token');
+      Cookie.remove('user_id');
+
+      setTimeout(() => {
+        router.push('/login');
+      }, 2000);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [message]);
+
+  return message ? (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p>{message} Redirecionando...</p>
+    </div>
+  ) : (
     <main className="h-screen">
       <div className="flex flex-col items-center justify-center h-full gap-8">
-        <SalutionContainer/>
-        <NavigateProfile/>
+        <SalutionContainer />
+        <NavigateProfile />
       </div>
     </main>
-  )
+  );
 }
