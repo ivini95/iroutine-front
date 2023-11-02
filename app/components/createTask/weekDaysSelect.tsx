2ce29e0a4099a9fddff4b30df109599ed0e7aed7
ivini95 <viniximenesdev@gmail.com>
2023-11-02 19:26:44 -0300

    refact: pass week days selected to create task container page

--- app/components/createTask/weekDaysSelect.tsx
+++ app/components/createTask/weekDaysSelect.tsx
@@ -1,15 +1,47 @@
-import { Checkbox } from "../ui/checkbox"
+'use client'
 
-export default function WeekDaysSelect() {
-    return (
-            <div className="flex gap-3">
-                <div data-testid="Sunday" className="flex w-6 h-6 relative"><Checkbox className="absolute w-5 h-5 border-2"></Checkbox><span className="h-4 w-4 leading-5 ml-1 font-semibold">D</span></div>
-                <div data-testid="Monday" className="flex w-6 h-6 relative"><Checkbox className="absolute w-5 h-5 border-2"></Checkbox><span className="h-4 w-4 leading-5 ml-1.5 font-semibold">S</span></div>
-                <div data-testid="Tuesday" className="flex w-6 h-6 relative"><Checkbox className="absolute w-5 h-5 border-2"></Checkbox><span className="h-4 w-4 leading-5 ml-1.5 font-semibold">T</span></div>
-                <div data-testid="Wednesday" className="flex w-6 h-6 relative"><Checkbox className="absolute w-5 h-5 border-2"></Checkbox><span className="h-4 w-4 leading-5 ml-1 font-semibold">Q</span></div>
-                <div data-testid="Thursday" className="flex w-6 h-6 relative"><Checkbox className="absolute w-5 h-5 border-2"></Checkbox><span className="h-4 w-4 leading-5 ml-1 font-semibold">Q</span></div>
-                <div data-testid="Friday" className="flex w-6 h-6 relative"><Checkbox className="absolute w-5 h-5 border-2"></Checkbox><span className="h-4 w-4 leading-5 ml-1.5 font-semibold">S</span></div>
-                <div data-testid="Saturday" className="flex w-6 h-6 relative"><Checkbox className="absolute w-5 h-5 border-2"></Checkbox><span className="h-4 w-4 leading-5 ml-1.5 font-semibold">S</span></div>
-            </div>
-    )
+import { Checkbox } from '../ui/checkbox';
+
+export default function WeekDaysSelect(props: any) {
+
+  return (
+    <div  className="flex gap-3">
+    
+      <div  data-testid="Sunday" className="flex w-6 h-6 relative">
+        <Checkbox onCheckedChange={(checked) =>{ props.setDaysSelected({...props.daysSelected, sun: checked})
+        }} className="absolute w-5 h-5 border-2"></Checkbox>
+        <span className="h-4 w-4 leading-5 ml-1 font-semibold">D</span>
+      </div>
+      <div data-testid="Monday" className="flex w-6 h-6 relative">
+        <Checkbox onCheckedChange={(checked) =>{ props.setDaysSelected({...props.daysSelected, mon: checked})
+        }} className="absolute w-5 h-5 border-2"></Checkbox>
+        <span className="h-4 w-4 leading-5 ml-1.5 font-semibold">S</span>
+      </div>
+      <div data-testid="Tuesday" className="flex w-6 h-6 relative">
+        <Checkbox onCheckedChange={(checked) =>{ props.setDaysSelected({...props.daysSelected, tue: checked})
+        }} className="absolute w-5 h-5 border-2"></Checkbox>
+        <span className="h-4 w-4 leading-5 ml-1.5 font-semibold">T</span>
+      </div>
+      <div data-testid="Wednesday" className="flex w-6 h-6 relative">
+        <Checkbox onCheckedChange={(checked) =>{ props.setDaysSelected({...props.daysSelected, wed: checked})
+        }} className="absolute w-5 h-5 border-2"></Checkbox>
+        <span className="h-4 w-4 leading-5 ml-1 font-semibold">Q</span>
+      </div>
+      <div data-testid="Thursday" className="flex w-6 h-6 relative">
+        <Checkbox onCheckedChange={(checked) =>{ props.setDaysSelected({...props.daysSelected, thu: checked})
+        }} className="absolute w-5 h-5 border-2"></Checkbox>
+        <span className="h-4 w-4 leading-5 ml-1 font-semibold">Q</span>
+      </div>
+      <div data-testid="Friday" className="flex w-6 h-6 relative">
+        <Checkbox onCheckedChange={(checked) =>{ props.setDaysSelected({...props.daysSelected, fri: checked})
+        }} className="absolute w-5 h-5 border-2"></Checkbox>
+        <span className="h-4 w-4 leading-5 ml-1.5 font-semibold">S</span>
+      </div>
+      <div data-testid="Saturday" className="flex w-6 h-6 relative">
+        <Checkbox onCheckedChange={(checked) =>{ props.setDaysSelected({...props.daysSelected, sat: checked})
+        }} className="absolute w-5 h-5 border-2"></Checkbox>
+        <span className="h-4 w-4 leading-5 ml-1.5 font-semibold">S</span>
+      </div>
+    </div>
+  );
 }
--- app/createTask/page.tsx
+++ app/createTask/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useEffect, useState } from 'react';
 import BackPage from '../components/backPage';
 import DateSelect from '../components/createTask/dateSelect';
 import DescriptionTask from '../components/createTask/descriptionTask';
@@ -5,12 +8,29 @@ import TypeOfTask from '../components/createTask/typeOfTask';
 import WeekDaysSelect from '../components/createTask/weekDaysSelect';
 import { Button } from '../components/ui/button';
 
+
 export default function CreateTask() {
+
+  const [daysSelected, setDaysSelected] = useState<any>({
+    sun: false,
+    mon: false,
+    tue: false,
+    wed: false,
+    thu: false,
+    fri: false,
+    sat: false,
+  }) 
+
+  useEffect(()=>{
+    console.log(daysSelected);
+    
+  },[daysSelected])
+
   return (
     <div className="flex flex-col items-center h-screen">
       <div className="flex flex-col items-center mt-28 gap-8">
         <BackPage />
-        <WeekDaysSelect />
+        <WeekDaysSelect setDaysSelected={setDaysSelected} daysSelected={daysSelected}/>
         <DescriptionTask />
         <TypeOfTask />
         <DateSelect />
